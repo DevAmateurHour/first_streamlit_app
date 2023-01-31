4c7ea63f5f5fe37271266132c802828af939ffc2
DevAmateurHour <markoppenheim@gmail.com>
2023-01-31 14:08:38 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
-streamlit.text('hello1')
-streamlit.title('hello2')
-
+streamlit.header('breakfast favourites')
+streamlit.text('🥣cerial')
 
 
